5b4944b0ae6e9cefeabcf9d5b63628c901736e56
RahmatAT11 <63140592+RahmatAT11@users.noreply.github.com>
2020-08-29 18:02:14 +0700

    Update on Project N
    Beberapa perubahan pada project Unitynya

---
+++ Project N/Assets/Scripts/CountDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    private int seconds = 305;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GameTimer", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GameTimer()
+    {
+        Debug.Log(seconds);
+        seconds--;
+        if (seconds == 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Project N/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 movement;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private ItemInformation itemCheckInfo;
+
+    public bool checkInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemCheckInfo = GameObject.Find("Item").GetComponent<ItemInformation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.E) && checkInfo)
+        {
+            Debug.Log(itemCheckInfo.itemType);
+            Debug.Log(itemCheckInfo.ethnicOrigin);
+            Debug.Log(itemCheckInfo.itemDescription);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector3(horizontalMovement, 0, verticalMovement);
+
+        transform.Translate(movement * Time.deltaTime * moveSpeed, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickupItem"))
+        {
+            checkInfo = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickupItem"))
+        {
+            checkInfo = false;
+        }
+    }
+}
