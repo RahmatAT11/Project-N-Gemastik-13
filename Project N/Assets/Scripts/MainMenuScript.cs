ba607345af320127992a858147806cb2e66adcf1
RahmatAT11 <63140592+RahmatAT11@users.noreply.github.com>
2020-09-03 13:47:22 +0700

    Navigasi Main Menu
    Untuk navigasi Main menu sudah ada.

---
+++ Project N/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Stage_Select_Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!!");
+        Application.Quit();
+    }
+}
